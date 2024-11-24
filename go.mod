d4c798251d6fdde14b713c2e2138c43845a76786
Yuuki1578 <destuawang@gmail.com>
2024-11-24 20:17:59 +0700

    Adding vector utilities

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yuuki1578/go-utils
+
+go 1.23.2
---
+++ vector.go
@@ -0,0 +1,143 @@
+package vector
+
+import "errors"
+
+// Compile-time constant error for attempting nil value.
+const NIL_VALUE_ACCESS string = "Error: attempting to access nil value"
+
+// Basic vector, capable of appending, popping, removing, etc.
+type Vector[T any] struct {
+	__slice []T
+	__len   uint64
+	__cap   uint64
+}
+
+// Get the vector length
+func (this *Vector[T]) Len() uint64 {
+	if this != nil {
+		return this.__len
+	}
+
+	return 0
+}
+
+// Get the vector capacity
+func (this *Vector[T]) Cap() uint64 {
+	if this != nil {
+		return this.__cap
+	}
+
+	return 0
+}
+
+// Update the length and capacity of the vector.
+func (this *Vector[T]) updateStatus() {
+	if this.__slice == nil {
+		return
+	}
+
+	this.__len = uint64(len(this.__slice))
+	this.__cap = uint64(cap(this.__slice))
+}
+
+// Initialize a vector with default capacity and allocates the slices with the capacity provided first.
+// In fact, this function is an abstraction over builtin function 'make'.
+func WithCapacity[T any](capacity uint64) Vector[T] {
+	return Vector[T]{
+		__slice: make([]T, 0, capacity),
+		__len:   0,
+		__cap:   capacity,
+	}
+}
+
+// Initialize a vector, it doesn't allocate the memory yet.
+// The initial capacity is 0. If you want to specify the default
+// capacity, use 'WithCapacity' instead.
+func New[T any]() Vector[T] {
+	return WithCapacity[T](0)
+}
+
+// Adding additional capacity to vector, if the pointer to instance (that called by the method)
+// is somehow 'nil', it return error. If it's NOT nil, it return nil instead.
+func (this *Vector[T]) AddCapacity(capacity uint64) error {
+	var safeCopy []T
+
+	if this == nil {
+		return errors.New(NIL_VALUE_ACCESS)
+	}
+
+	if this.__slice == nil {
+		*this = WithCapacity[T](capacity)
+		return nil
+	}
+
+	var _ = copy(safeCopy, this.__slice)
+
+	this.__slice = nil
+	*this = WithCapacity[T](uint64(cap(safeCopy) + int(capacity)))
+	this.__slice = append(this.__slice, safeCopy...)
+	this.updateStatus()
+
+	safeCopy = nil
+
+	return nil
+}
+
+// Clear the vector, truncating it to initialization / zero value.
+func (this *Vector[T]) Clear() {
+	if this == nil {
+		return
+	}
+
+	this.__slice = nil
+	*this = New[T]()
+	this.updateStatus()
+}
+
+// Appending element from the back, return error if the instance is nil.
+// On success, return nil instead.
+func (this *Vector[T]) Append(element T) error {
+	if this == nil {
+		return errors.New(NIL_VALUE_ACCESS)
+	}
+
+	if this.__slice == nil {
+		*this = New[T]()
+	}
+
+	this.__slice = append(this.__slice, element)
+	this.updateStatus()
+
+	return nil
+}
+
+func (this *Vector[T]) Pop(index uint64) (T, error) {
+	var (
+		defaultValue T
+		leftSide     []T = nil
+		rightSide    []T = nil
+		safeCopy     []T = nil
+		_            int
+	)
+
+	if this == nil || this.__slice == nil || index >= this.Len() {
+		return defaultValue, errors.New(NIL_VALUE_ACCESS)
+	}
+
+	defaultValue = this.__slice[index]
+	leftSide = this.__slice[:index]
+	rightSide = this.__slice[index+1:]
+
+	safeCopy = make([]T, 0, this.Cap()-1)
+	safeCopy = append(safeCopy, leftSide...)
+	safeCopy = append(safeCopy, rightSide...)
+
+	this.Clear()
+
+	_ = copy(this.__slice, safeCopy)
+	this.updateStatus()
+
+	safeCopy = nil
+
+	return defaultValue, nil
+}
